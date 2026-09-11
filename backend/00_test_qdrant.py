4c70bac9f0fee53a40ee7caaf2af63cccaaf4649
Luitz Daryl PH <luitzdaryl20@gmail.com>
2026-09-11 14:35:56 +0800

    Updated folder

--- backend/00_test_qdrant.py
+++ backend/00_test_qdrant.py
@@ -3,9 +3,10 @@ from qdrant_client.models import Distance, VectorParams
 
 client = QdrantClient(host="localhost", port=6333)
 
-client.recreate_collection(
-    collection_name="test",
-    vectors_config=VectorParams(size=4, distance=Distance.COSINE),
-)
+if not client.collection_exists("test"):
+    client.create_collection(
+        collection_name="test",
+        vectors_config=VectorParams(size=4, distance=Distance.COSINE),
+    )
 
 print("Collections:", client.get_collections())
